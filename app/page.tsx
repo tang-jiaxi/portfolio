685e1d72d738a276577cf21c07b9aa3d0d04129b
Jiaxi Tang <jt.jiaxitang@gmail.com>
2025-01-09 14:42:00 +0800

    update hero message

--- app/page.tsx
+++ app/page.tsx
@@ -52,8 +52,7 @@ const Home = () => {
           </h1>
           <h2 className="pt-4 text-black font-normal md:pt-6 lg:text-[1.6vw] lg:leading-[2.5vw] inter:max-w-[55vw] md:max-w-[45vw] lg:max-w-[40vw]">
             I’m pursuing a <span style={underlineStyle}> dual degree in Computer Science and
-            Graphic Design with a minor in UX Design.</span> Currently the
-            President of design consultancy club, Design U!
+            Graphic Design with a minor in UX Design.</span> Passionate about building objectively awesome solutions!
           </h2>
           <h4 className="pt-[15vh] z-0">Drag and drop the logos for some fun!</h4>
         </div>
---
+++ components/ProjectIntro.tsx
@@ -0,0 +1,67 @@
+"use client"
+
+import { usePathname } from "next/navigation";
+import { ProjectsArray } from "./ProjectsArray"
+import Tag from "./Tag";
+
+interface ProjectIntroProps {
+  date: string;
+  topRight: string[];
+  topLeft: string[];
+  bottomRight: string[];
+  bottomLeft: string[];
+}
+
+export const ProjectIntro: React.FC<ProjectIntroProps> = ({date, topRight, topLeft, bottomRight, bottomLeft }) => {
+  const pathname = usePathname();
+  const projectData = ProjectsArray.find((project) => project.link === pathname) || {
+    name: '',
+    title: '',
+    tags: [],
+    hashtags: [],
+  };
+  
+  return (
+    <section className="w-[90vw] md:w-[93vw] lg:w-[90vw] mx-auto flex flex-col md:flex-row md:basis-1/12 border border-black rounded-[20px] md:rounded-[30px]">
+      
+      <div className="border-b md:border-0 border-black flex flex-col gap-4 md:gap-6 p-4 md:p-6">
+        <div>
+          <h1>{projectData.title}</h1>
+          <p>{date}</p>
+        </div>
+
+        <div className="flex flex-col gap-3 md:gap-4">
+          <div className="flex flex-wrap lg:flex-nowrap gap-2 justify-start">
+            {projectData.tags.map((tag, index) => (
+              <Tag key={index} tag={tag}/>
+            ))}
+          </div>
+          <div className="flex flex-wrap gap-x-2 md:gap-x-4 justify-start text-gray-500">
+            {projectData.hashtags.map((hashtag, index) => (
+              <h3 key={index}>{hashtag}</h3>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 gap-4 p-4 footer:p-0 footer:gap-0 footer:grid-cols-2 md:grid-cols-2 md:basis-11/12">
+        <div className="footer:border-b footer:border-r md:border-l border-black footer:p-4 md:p-6 ">
+          <h5 className="text-black">{topRight[0]}</h5>
+          <p className="m-0">{topRight[1]}</p>
+        </div>
+        <div className="footer:border-b border-black footer:p-4 md:p-6 ">
+          <h5 className="text-black">{topLeft[0]}</h5>
+          <p className="m-0">{topLeft[1]}</p>
+        </div>
+        <div className="footer:border-r md:border-l border-black footer:p-4 md:p-6 ">
+          <h5 className="text-black">{bottomLeft[0]}</h5>
+          <p className="m-0">{bottomLeft[1]}</p>
+        </div>
+        <div className="footer:p-4 md:p-6 ">
+          <h5 className="text-black">{bottomRight[0]}</h5>
+          <p className="m-0">{bottomRight[1]}</p>
+        </div>
+      </div>
+    </section>
+  )
+}
