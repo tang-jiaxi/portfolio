4980fd907c419112bc9698304b5f591770331393
Jiaxi Tang <jt.jiaxitang@gmail.com>
2025-01-14 03:05:39 +0800

    escape characters

--- app/(casestudy)/radioK/_components/9_Insight2.tsx
+++ app/(casestudy)/radioK/_components/9_Insight2.tsx
@@ -4,9 +4,9 @@ export default function Insight2() {
   const text = (
     <>
       <h1>Insight 2</h1>
-      <p>Users are confused by the podcast’s lack of theme.</p>
+      <p>Users are confused by the podcast&apos;s lack of theme.</p>
       <blockquote>&quot;It seems like a mix of everything for things surrounding college. Kind of confused about the theme.&quot;</blockquote>
-      <blockquote>&quot;There's a lot of different things going on. The content is very confusing and not cohesive.&quot;</blockquote>
+      <blockquote>&quot;There&apos;s a lot of different things going on. The content is very confusing and not cohesive.&quot;</blockquote>
     </>
   )
 
